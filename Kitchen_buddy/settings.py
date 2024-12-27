f77859ef97bf48987c7ba6f2405564e13a1b332b
iliana-miladinova <iliana.miladinova@abv.bg>
2024-12-28 00:44:59 +0200

    Registration form disaign

--- Kitchen_buddy/settings.py
+++ Kitchen_buddy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "Users/static",
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -138,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Users/views.py
+++ Users/views.py
@@ -20,21 +20,21 @@ def register_view(request):
 
         if password != confirm_password:
             messages.error(request, 'Passwords do not match')
-            return render(request, 'User/registration.html')
+            return render(request, 'Users/registration.html')
 
         if User.objects.filter(username=username).exists():
             messages.error(request, 'An acount with this username already exists')
-            return render(request, 'User/registration.html')
+            return render(request, 'Users/registration.html')
         
         if User.objects.filter(email=email).exists():
             messages.error(request, 'This email is already used')
-            return render(request, 'User/registration.html')
+            return render(request, 'Users/registration.html')
         
         User.objects.create_user(username=username, email=email, password=password)
         messages.success(request, 'Your account was created successfuly! Please, login.')
         return redirect('login')
     
-    return render(request, 'User/registration.html')
+    return render(request, 'Users/registration.html')
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -50,7 +50,7 @@ def login_view(request):
             redirect('home')
         else:
             messages.error(request, 'Incorrect username or password')
-            return render(request, 'User/login.html')
+            return render(request, 'Users/login.html')
         
     return render(request, 'Users/login.html')
 
