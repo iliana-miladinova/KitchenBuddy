a578cf82d0b0abbb72dc748cdff822349fdb9b31
iliana-miladinova <iliana.miladinova@abv.bg>
2025-02-15 14:45:37 +0200

    Template fixes

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import IngredientsDetails
+from ingredients.models import Ingredient
 
 class IngredientsDetailsForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['ingredient'].queryset = Ingredient.objects.all().order_by('category', 'name')
+
+        
     class Meta:
         model = IngredientsDetails
         fields = ['ingredient', 'quantity', 'amount']
